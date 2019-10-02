62d51e3b100dd301c9e79e1e8074bba18d9d1bb4
Alencruser <alencruserxx@hotmail.fr>
2019-10-02 20:07:19 +0200

    todolist fonctionnelle sans remove des elements

--- todo.js
+++ todo.js
@@ -3,11 +3,29 @@ import { LitElement, html } from 'lit-element';
 
 class Todo extends LitElement {
 
+    constructor() {
+        super()
+        this.todolist=[];
+        this.task="";
+    }
+
     render() {
         return html`
-        <p>Hello !</p>
+        <input type="text" placeholder="Tâche" .value="${this.task}" @input=${this.handleInput}>
+        <button @click="${this.appendToList}">Ajouter à la liste</button> 
+        <ul>${this.todolist.map(item => html`<li>${item}</li>`)}</ul>
         `;
     }
+    appendToList(){
+        this.todolist.push(this.task);
+        console.log(this.todolist);
+        this.requestUpdate();
+    }
+
+    handleInput(e){
+        this.task = e.target.value;
+    }
+
 }
 
 customElements.define('todo-strike', Todo);
